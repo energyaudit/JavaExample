d2d0a78bf2372f7de99ee1c3580b348c58817195
energyaudit <energyaudit668@gmail.com>
2020-04-12 18:40:05 -0400

    [billy] change xmltest to json restassured

--- src/main/RestAssured/JsonXmlTest.java
+++ src/main/RestAssured/JsonXmlTest.java
@@ -2,7 +2,7 @@ package src.main.RestAssured;
 
 
         import static io.restassured.RestAssured.given;
-
+        import java.util.Random;
         import org.hamcrest.Matchers;
         import org.testng.Assert;
         import org.testng.annotations.Test;
@@ -10,29 +10,37 @@ package src.main.RestAssured;
         import io.restassured.http.ContentType;
         import io.restassured.response.Response;
 
-public class xmlTest {
+public class JsonXmlTest {
 
 
     @Test
     public void post_xml_test() {
+        org.apache.log4j.BasicConfigurator.configure();
+        Random random = new Random();
+        String requestBody =
+                "{\"name\": \"Lisa "+random.nextInt()+"Tamaki\"}";
         Response response = given().
                 contentType(ContentType.XML)
-                .accept(ContentType.XML)
-                .body("<empRequest>\n" +
-                        "	<id>1</id>\n" +
-                        "	<name>PK</name>\n" +
-                        "</empRequest>")
+        .contentType(ContentType.JSON)
+//                .accept(ContentType.XML)
+//                .body("<empRequest>\n" +
+//                        "	<id>1</id>\n" +
+//                        "	<name>PK</name>\n" +
+//                        "</empRequest>")
+//                .body("{\"name\": \"Lisa Tamaki\"}")
+                .body(requestBody)
                 .when()
-                .post("http://localhost:8080/My-Jersey-Project/rest/emp/getEmp");
+                .post("http://localhost:8080/actor");
         System.out.println("POST Response\n" + response.asString());
         // tests
         Assert.assertEquals(response.getStatusCode(),200);
-        response.then().body("empResponse.id", Matchers.is("1"));
-        response.then().body("empResponse.name", Matchers.is("PK"));
+//        response.then().body("empResponse.id", Matchers.is("1"));
+//        response.then().body("empResponse.name", Matchers.is("PK"));
     }
 
     @Test
     public void post_xml_error_test() {
+        org.apache.log4j.BasicConfigurator.configure();
         Response response = given().
                 contentType(ContentType.XML)
                 .accept(ContentType.XML)
---
+++ src/main/RestAssured/xmlTest1.java
@@ -0,0 +1,46 @@
+package main.RestAssured;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.Random;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class xmlTest1 {
+
+    @Test
+    public void xmlPostRequest_Test() {
+        org.apache.log4j.BasicConfigurator.configure();
+        RestAssured.baseURI = "http://localhost:8080";
+
+//        String requestBody = "<client>\r\n" +
+//                "    <clientNo>100</clientNo>\r\n" +
+//                "    <name>Tom Cruise</name>\r\n" +
+//                "    <ssn>124-542-5555</ssn>\r\n" +
+//                "</client>";
+        Random random = new Random();
+        String requestBody =
+                "{\"name\": \"Lisa "+random.nextInt()+"Tamaki\"}";
+
+        Response response = null;
+
+        response = given().
+//                contentType(ContentType.XML)
+           contentType(ContentType.JSON)
+                .accept(ContentType.XML)
+                .body(requestBody)
+                .when()
+//                .post("/addClient");
+                .post("/actor");
+
+        System.out.println("Post Response :" + response.asString());
+        System.out.println("Status Code :" + response.getStatusCode());
+//        System.out.println("Does Reponse contains 'Lisa Tamaki'? :" + response.asString().contains("Lisa Tamaki"));
+        Assert.assertEquals(response.getStatusCode(),200);
+    }
+
+}
