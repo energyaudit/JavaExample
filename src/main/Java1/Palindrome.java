541d2e9e58910a71acb1c83eb20374ef174ff33f
energyaudit <energyaudit668@gmail.com>
2020-04-22 00:18:16 -0400

    [billy] Updated Palindrome

--- src/main/Java1/Palindrome.java
+++ src/main/Java1/Palindrome.java
@@ -1,7 +1,6 @@
 package main.Java1;
 
 import java.util.Scanner;
-
 public class Palindrome {
     static boolean  isPalindrome(String str) {
         String  rev = "";
@@ -9,27 +8,30 @@ public class Palindrome {
 
         for ( int i = length - 1; i >= 0; i-- )
             rev = rev + str.charAt(i);
-
         if (str.equals(rev))
-//            System.out.println(str+" is a palindrome");
             return true;
         else
-//            System.out.println(str+" is not a palindrome");
         return false;
     }
     public static void main(String args[])
     {
         String str1 = "ROTOR";
 //        Scanner sc = new Scanner(System.in);
-//
 //        System.out.println("Enter a string:");
 //        str1 = sc.nextLine();
         boolean Result;
         Result=isPalindrome(str1);
         System.out.println("The string is: "+ Result);
+        String[] testStrings = { "kayak", "refer", "mom", "dog", "levels", "no more", "longest", "civic", "madam", "reviver", "reader", "top spot"};
+        int length = testStrings.length;
+        for ( int i = length - 1; i >= 0; i-- ){
+            System.out.println(testStrings[i]+" is: "+ isPalindrome(testStrings[i]));
+        }
+        Integer[] testNumber={10101,10201,10301,10501,10601,10801};
+        int length1 = testNumber.length;
+        for ( int i = length1 - 1; i >= 0; i-- ){
+            String j=testNumber[i].toString();
+            System.out.println(testNumber[i]+" is: "+ isPalindrome(j));
+        }
     }
-
-
-
-
 }
