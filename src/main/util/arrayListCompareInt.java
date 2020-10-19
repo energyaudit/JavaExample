bbf5f2407f9d455e1dfa90a96274a250077187fa
BillyY1 <billy.yang@verifone.com>
2020-10-19 08:09:03 -0400

    [billy]compare list to integer

--- src/main/util/arrayListCompareInt.java
+++ src/main/util/arrayListCompareInt.java
@@ -1,42 +1,46 @@
 package main.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class arrayListCompareInt {
 
     public static Boolean arrayListCompareANInt(ArrayList<String> listStr, String operator, Integer benchmark) {
-        Boolean compare = true;
+        Boolean compare = false;
         switch (operator) {
-            case "<":
+            case "<=":
+                ArrayList<Integer> listInt = new ArrayList<Integer>();
                 for (String str : listStr) {
-                    if (Integer.parseInt(str) >= benchmark) {
-                        compare = false;
-                        break;
-                    }
+                    listInt.add(Integer.parseInt(str));
                 }
+                System.out.println("max in list is :" + Collections.max(listInt));
+                if (Collections.max(listInt) <= benchmark) {
+                    compare = true;
 
-            case ">":
+                }
+                break;
+            case ">=":
+                ArrayList<Integer> listInt1 = new ArrayList<Integer>();
                 for (String str1 : listStr) {
-
-                    if (Integer.parseInt(str1) <= benchmark) {
-                        System.out.println(Integer.parseInt(str1));
-                        compare = false;
-                        break;
-                    }
-
+                    listInt1.add(Integer.parseInt(str1));
                 }
+                if (Collections.max(listInt1) >= benchmark) {
+                    compare = true;
+                }
+                break;
             case "=":
-                for (String str1 : listStr) {
-                    if (Integer.parseInt(str1) < benchmark||Integer.parseInt(str1) > benchmark) {
+                for (String str2 : listStr) {
+                    if (Integer.parseInt(str2) < benchmark || Integer.parseInt(str2) > benchmark) {
                         compare = false;
+                    } else {
+                        compare = true;
                     }
                     break;
                 }
-        }
-        System.out.println("The whole arraylist"+listStr+operator+benchmark+" :  "+compare);
+            }
+        System.out.println("The whole arraylist" + listStr + operator + benchmark + " :  " + compare);
         return compare;
-        }
-
+    }
     public static void main(String[] args) {
         ArrayList<String> listStr = new ArrayList<>();
         listStr.add("9");
@@ -45,9 +49,13 @@ public class arrayListCompareInt {
         listStr.add("8");
         listStr.add("7");
         listStr.add("8");
-//        arrayListCompareANInt(listStr,"<",8);
-        arrayListCompareANInt(listStr,">",5);
-        arrayListCompareANInt(listStr,">",0);
+        arrayListCompareANInt(listStr,">=",8);
+        arrayListCompareANInt(listStr,">=",9);
+        arrayListCompareANInt(listStr,">=",10);
+        arrayListCompareANInt(listStr,"<=",10);
+        arrayListCompareANInt(listStr,"<=",9);
+        arrayListCompareANInt(listStr,"<=",6);
+        arrayListCompareANInt(listStr,"<=",0);
         ArrayList<String> listStr1 = new ArrayList<>();
         listStr1.add("8");
         listStr1.add("8");
